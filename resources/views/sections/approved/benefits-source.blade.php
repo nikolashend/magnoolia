f1a8d9890c8c7bd7cc2cf6c0634eff8fd6f41329
vadim.markin <vadim.markin.1979.05@gmail.com>
2026-05-25 23:15:13 +0300

    block 3

--- resources/views/sections/approved/benefits-source.blade.php
+++ resources/views/sections/approved/benefits-source.blade.php
@@ -4,57 +4,57 @@
             <div class="sec-title text-center">
                 <div class="sec-title__top justify-content-center">
                     <span class="line-left"></span>
-                    <h6 class="sec-title__tagline bw-split-in-right">Our Best Service</h6>
+                    <h6 class="sec-title__tagline bw-split-in-right">Magnoolia eelised</h6>
                     <span class="line-right"></span>
                 </div>
-                <h3 class="sec-title__title bw-split-in-left">Property Management Services <br> Managing of rental</h3>
+                <h3 class="sec-title__title bw-split-in-left">Kõik, mida vajad. <br> Ühes kohas.</h3>
             </div>
             <div class="row gutter-y-30">
                 <div class="col-lg-3 col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="100ms">
                     <div class="service-card">
-                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-kitchen"></i></span></div>
+                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-flooring"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Kitchens Room</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">Põrandaküte</a></h3>
+                            <p class="service-card__text">Veesoojendusega põrandaküte igas toas, sh vannitoas</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">
                     <div class="service-card">
-                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-restaurant"></i></span></div>
+                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-house"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Restaurant</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">Ventilatsioon</a></h3>
+                            <p class="service-card__text">Soojusvahestiga ventilatsioonisüsteem tagab puhta sisekliima</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
                 </div>
                 <div class="col-lg-6 col-md-12 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="500ms">
                     <div class="service-card__image hover:shine">
-                        <img src="{{ asset('assets/images/services/service-1-1.jpg') }}" alt="image">
+                        <img src="{{ asset('assets/images/magnoolia/Interior 1.jpg') }}" alt="Magnoolia sisevaade" style="width:569px;height:347px;object-fit:cover;display:block;">
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="100ms">
                     <div class="service-card">
-                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-bath"></i></span></div>
+                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-labyrinth"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Bath Room</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">Privaatne hooviala</a></h3>
+                            <p class="service-card__text">Igal kodil on oma piiratud ja privaatne aed</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">
                     <div class="service-card">
-                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-bedroom"></i></span></div>
+                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-real-estate"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Bed Room</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">Rõdu ja terrass</a></h3>
+                            <p class="service-card__text">Iga kodu juurde kuulub isiklik terrass ja rõdu</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
@@ -63,20 +63,20 @@
                     <div class="service-card">
                         <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-garage"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Parking Car</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">EV laadimine</a></h3>
+                            <p class="service-card__text">Elektriautode laadimispunkti ettevalmistus iga parkimiskoha juurde</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
                 </div>
                 <div class="col-lg-3 col-md-6 col-sm-6 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="700ms">
                     <div class="service-card">
-                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-dumbbell"></i></span></div>
+                        <div class="service-card__icon-box"><span class="service-card__icon"><i class="icon-trophy"></i></span></div>
                         <div class="service-card__content"><div class="service-card__content__inner">
-                            <h3 class="service-card__title"><a href="#">Fitness Center</a></h3>
-                            <p class="service-card__text">Home tailored design service sed do bulding room</p>
-                            <a href="#" class="service-card__link">READ MORE<i class="icon-angle-small-right"></i></a>
+                            <h3 class="service-card__title"><a href="{{ route('apartments.index') }}">A-energiaklass</a></h3>
+                            <p class="service-card__text">Maasoojuspump ja päikesepaneelide valmidus A-klassi kodu</p>
+                            <a href="{{ route('apartments.index') }}" class="service-card__link">LÄHEMALT<i class="icon-angle-small-right"></i></a>
                         </div></div>
                         <div class="service-card__shape"></div>
                     </div>
