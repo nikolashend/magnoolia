fe5b3e8a005c01b5ec1f2fadb22aed2ad49cc0b8
vadim.markin <vadim.markin.1979.05@gmail.com>
2026-05-25 22:47:14 +0300

    block 2

--- resources/views/sections/approved/about-magnoolia-source.blade.php
+++ resources/views/sections/approved/about-magnoolia-source.blade.php
@@ -1,22 +1,22 @@
 {{-- SOURCE: php-template/parts/home1/about.php | class: about-one section-space --}}
-    <section class="about-one section-space" id="about">
+    <section class="about-one section-space" id="about" style="background-color:#ffffff;">
         <div class="container">
             <div class="row gutter-y-50">
                 <div class="col-lg-6 wow fadeInLeft" data-wow-duration="1500ms" data-wow-delay="300ms">
                     <div class="about-one__image">
                         <div class="about-one__image__item">
-                            <div class="about-one__image__item__inner hover:shine">
-                                <img src="{{ asset('assets/images/about/about-1-1.jpg') }}" alt="about" class="about-one__image__one">
+                            <div class="about-one__image__item__inner hover:shine" style="overflow:hidden;width:409px;height:480px;">
+                                <img src="{{ asset('assets/images/magnoolia/Cam004.0000.jpg') }}" alt="Magnoolia ridaelamud välisvaade" class="about-one__image__one" style="width:409px;height:480px;object-fit:cover;object-position:center;display:block;">
                             </div>
                             <div class="about-one__experience wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">
-                                <h3 class="about-one__experience__title">Since 1972</h3>
+                                <h3 class="about-one__experience__title">Valmib 2027</h3>
                             </div>
                         </div>
                         <div class="about-one__image__item-two hover:shine">
                             <div class="about-one__image__icon">
                                 <img src="{{ asset('assets/images/shapes/house-1-1.png') }}" alt="icon">
                             </div>
-                            <img src="{{ asset('assets/images/about/about-s-1-1.jpg') }}" alt="about" class="about-one__image__one">
+                            <img src="{{ asset('assets/images/magnoolia/Interior 4.jpg') }}" alt="Magnoolia sisevaade" class="about-one__image__one" style="width:336px;height:429px;object-fit:cover;display:block;">
                         </div>
                     </div>
                 </div>
@@ -25,20 +25,20 @@
                         <div class="sec-title text-start">
                             <div class="sec-title__top justify-content-start">
                                 <span class="line-left"></span>
-                                <h6 class="sec-title__tagline bw-split-in-right">Our About property</h6>
+                                <h6 class="sec-title__tagline bw-split-in-right">Miks Magnoolia?</h6>
                             </div>
-                            <h3 class="sec-title__title bw-split-in-left">We Provide thorough About <br> customer service Home.</h3>
+                            <h3 class="sec-title__title bw-split-in-left">Läbimõeldud kodu. <br> Privaatne hooviala.</h3>
                         </div>
-                        <p class="about-one__text wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">Home tailored design, management &amp; support services business agency elit, sed do eiusmod tempor majority have in some we form by injected humour solution.</p>
+                        <p class="about-one__text wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">Magnoolia on uusarendus, kus iga detail on läbi mõeldud — alates A-energiaklassi lahendustest kuni privaatse planeeringuni. 19 iseseisvat kodu privaatse hooviala, terrassi ja rõduga.</p>
                         <div class="about-one__features">
                             <ul class="about-one__features__list list-unstyled">
-                                <li class="wow fadeInUp" data-wow-duration="1500ms"><i class="icon-check-star"></i><span>Magnificent project of three modern.</span></li>
-                                <li class="wow fadeInUp" data-wow-duration="1600ms"><i class="icon-check-star"></i><span>Movable items not fixed to land</span></li>
-                                <li class="wow fadeInUp" data-wow-duration="1700ms"><i class="icon-check-star"></i><span>a private plot and a swimming pool.</span></li>
+                                <li class="wow fadeInUp" data-wow-duration="1500ms"><i class="icon-check-star"></i><span>Maasoojuspump, ventilatsioon ja põrandaküte</span></li>
+                                <li class="wow fadeInUp" data-wow-duration="1600ms"><i class="icon-check-star"></i><span>Privaatne hooviala, terrass ja rõdu igale kodule</span></li>
+                                <li class="wow fadeInUp" data-wow-duration="1700ms"><i class="icon-check-star"></i><span>EV laadimise ja päikesepaneelide valmidus</span></li>
                             </ul>
                         </div>
                         <div class="about-one__btn">
-                            <a href="#" class="zoomvilla-btn">About More <i class="icon-angle-small-right"></i></a>
+                            <a href="{{ route('apartments.index') }}" class="zoomvilla-btn">Vaata kodusid <i class="icon-angle-small-right"></i></a>
                         </div>
                     </div>
                 </div>
