2162796e3677b977864f19df0aae40e6f1776f05
vadim.markin <vadim.markin.1979.05@gmail.com>
2026-06-25 20:05:16 +0300

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,11 @@
     @include('partials.seo.meta')
 
     <!-- Favicon Icons -->
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicons/apple-touch-icon.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}">
+    @php $favV = @filemtime(public_path('assets/images/favicons/favicon-32x32.png')) ?: 1; @endphp
+    <link rel="icon" href="{{ asset('favicon.ico') }}?v={{ $favV }}" sizes="any">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/images/favicons/apple-touch-icon.png') }}?v={{ $favV }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/images/favicons/favicon-32x32.png') }}?v={{ $favV }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/images/favicons/favicon-16x16.png') }}?v={{ $favV }}">
     <link rel="manifest" href="{{ asset('assets/images/favicons/site.webmanifest') }}">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
